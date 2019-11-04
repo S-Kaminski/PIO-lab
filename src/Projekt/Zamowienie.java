ee249c1add4dd2bfbba29f0ad35f7c642a1ed2d9
Simon Kaminski <szy.kaminski@gmail.com>
2019-11-04 16:53:08 +0100

    Created project package

---
+++ src/Projekt/Zamowienie.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Projekt;
+
+import java.util.List;
+
+/**
+ *
+ * @author User
+ */
+public class Zamowienie extends ZamowienieWew
+{
+    private List<Produkt> produkty;
+    
+    
+}
