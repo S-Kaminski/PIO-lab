5717b347ba6bc1547a385912b133d6210001a85c
Simon Kaminski <szy.kaminski@gmail.com>
2019-11-04 16:35:37 +0100

    Added MainClass to test functionality

--- src/SingletonP/OsobaSingleton.java
+++ src/SingletonP/OsobaSingleton.java
@@ -12,9 +12,37 @@ package SingletonP;
 public class OsobaSingleton 
 {
     private static OsobaSingleton instance;
-    private String imie = "";
-    private String nazwisko = "";
-    private String pesel = "";
+    private String imie;
+    private String nazwisko;
+    private String pesel;
+
+    public static void setInstance(OsobaSingleton instance) {
+        OsobaSingleton.instance = instance;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
     
  private OsobaSingleton(){}
  
